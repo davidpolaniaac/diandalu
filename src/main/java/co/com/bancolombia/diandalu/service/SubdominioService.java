1dcb8f5afcba200679486de9ccf768cb4bd1549e
David Polania <davidpolaniaac@users.noreply.github.com>
2017-02-06 11:37:57 -0500

    Editar un subdominio : Service y el Business

--- src/main/java/co/com/bancolombia/diandalu/service/SubdominioService.java
+++ src/main/java/co/com/bancolombia/diandalu/service/SubdominioService.java
@@ -1,13 +1,22 @@
 package co.com.bancolombia.diandalu.service;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+import co.com.bancolombia.business.SubdominioBusiness;
 import co.com.bancolombia.entidades.Subdominio;
 
+@Path("/subdominios")
 public class SubdominioService {
 	
+	@Inject
+	private SubdominioBusiness subdominioBusiness;
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -15,5 +24,20 @@ public class SubdominioService {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	 @GET  
+	 @Path("/{id}")  
+	 @Produces(MediaType.APPLICATION_JSON)  
+	 public Subdominio getSubdominioById(@PathParam("id") int id){  
+		 return subdominioBusiness.getSubdominio(id);  
+	 }  
+	
+	 @PUT  
+	 @Consumes(MediaType.APPLICATION_JSON)  
+	 public void updateSubdominio(Subdominio subdominio)  {  
+		subdominioBusiness.updateSubdominio(subdominio);  
+	 } 
+
+	
 
 }
